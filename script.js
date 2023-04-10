22967e3d6b4ec6ead99eda8a1bd1592c638937f0
FrantiskaAli <tiskarechk@gmail.com>
2023-04-10 22:32:54 +0100

    final working order

--- script.js
+++ script.js
@@ -6,29 +6,27 @@ const yearInput = document.getElementById("year");
 const days = document.getElementById("days")
 const months = document.getElementById("months")
 const years = document.getElementById("years")
-
+const error = document.getElementsByClassName("error")
 //function on  click
 button.addEventListener("click", function(){
 //getting values from the input
-const dayValue = dayInput.value;
-const monthValue = monthInput.value;
-const yearValue = yearInput.value;
+let dayValue = dayInput.value;
+let monthValue = monthInput.value;
+let yearValue = yearInput.value;
 const allInputs = [dayInput, monthInput, yearInput]; 
 //error
-//if (!dayValue || !monthValue )
 //subtracting moment js to get results
-for (let i = 0; i < allInputs.length; i++){
-    if (!allInputs[i].value.length){
-        allInputs[i].classList.remove("required")
-       allInputs[i].parentNode.empty()
-    }
-}
-
-if(dayValue && monthValue && yearValue){
+validate()
+//validation
+if(validate()){
+    for (let i = 0; i < allInputs.length; i++){
+    allInputs[i].classList.remove("required")
+    allInputs[i].nextElementSibling.classList.add("hide")
+    allInputs[i].previousElementSibling.classList.remove("error")}
 
 let age = moment().subtract({ days:dayValue, months: monthValue, years: yearValue});
 console.log(age.format("D, M , YY"))
-//changing dom to display resulte
+//changing dom to display results
 years.textContent = age.format("YY");
 months.textContent = age.format("M");
 days.textContent = age.format("D");
@@ -36,12 +34,38 @@ days.textContent = age.format("D");
     for (let i = 0; i < allInputs.length; i++){
         if (!allInputs[i].value.length){
             allInputs[i].classList.add("required")
-           allInputs[i].parentNode.append("required")
+            allInputs[i].nextElementSibling.classList.remove("hide")
+            allInputs[i].previousElementSibling.classList.add("error")
         }
     }
 }
 //styling invalid input
-
+function validate(){
+    let valid = 0
+     for (let i = 0; i < allInputs.length; i++){
+        if (dayValue > 31){
+            dayInput.classList.add("required")
+            dayInput.nextElementSibling.classList.remove("hide")
+            dayInput.previousElementSibling.classList.add("error")
+            valid++
+        }
+        if (monthValue > 12){
+            monthInput.classList.add("required")
+            monthInput.nextElementSibling.classList.remove("hide")
+            monthInput.previousElementSibling.classList.add("error")
+            valid++
+        }
+        if (yearValue > 2023){
+            yearInput.classList.add("required")
+            yearInput.nextElementSibling.classList.remove("hide")
+            yearInput.previousElementSibling.classList.add("error")
+            valid++
+        }
+    }
+    if (valid > 0){
+        return false
+    } else {return true}
+}
 })
 
 
